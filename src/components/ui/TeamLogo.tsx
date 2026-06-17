452459ce0285454eb59b3745c0259ddc385bddc8
Gustavo Eguez <eguezgustavo@gmail.com>
2026-06-17 14:21:49 -0500

    Guard TeamLogo against null/undefined src

--- src/components/ui/TeamLogo.tsx
+++ src/components/ui/TeamLogo.tsx
@@ -5,7 +5,7 @@ interface TeamLogoProps {
 }
 
 export function TeamLogo({ src, name, className = 'w-8 h-8' }: TeamLogoProps) {
-  if (src.startsWith('http')) {
+  if (src?.startsWith('http')) {
     return <img src={src} alt={name} className={`${className} object-contain`} />;
   }
   return <span className="text-2xl leading-none">{src}</span>;
